ad27a24bccc6e78f510062a1d35b5d299928dbad
PirvuStefan <silverbench24@gmail.com>
2025-08-20 20:22:16 +0300

    ba nu stiu ce pula mea

--- src/main/java/org/example/cec/Employee.java
+++ src/main/java/org/example/cec/Employee.java
@@ -3,12 +3,14 @@ package org.example.cec;
 public class Employee {
     String name; // numele angajatului
     int numberOfShifts; // numarul de ture care trebuie sa fie acoperite in luna respectiva
-    int shifts[];   // 0 gol, 1 weekend, 2 sarbatoare
+    int[] shifts = new int[20];   // 0 gol, 1 weekend, 2 sarbatoare
 
     Employee(String name, int numberOfShifts, int[] shifts) {
         this.name = name;
         this.numberOfShifts = numberOfShifts;
-        this.shifts = new int[numberOfShifts];
+        for(int i = 0; i < numberOfShifts; i++) {
+            this.shifts[i] = 0;
+        }
     }
 
 }
